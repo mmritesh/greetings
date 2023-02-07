2f1c94a915e278cb3c0fe0cf5018e9a987c93394
Ritesh <ritesh.manjaramkar@pearson.com>
2023-02-07 11:16:00 +0530

    greetings: changes for v0.1.0

---
+++ go.mod
@@ -0,0 +1,5 @@
+module pearson.com/dos/common/lib
+
+go 1.19
+
+replace pearson.com/dos/common/lib/pi_token_go => ../pi_token_go
---
+++ greetings.go
@@ -0,0 +1,10 @@
+package pi_token_go
+
+import "fmt"
+
+// Hello returns a greeting for the named person.
+func Hello(name string) string {
+	// Return a greeting that embeds the name in a message.
+	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	return message
+}
